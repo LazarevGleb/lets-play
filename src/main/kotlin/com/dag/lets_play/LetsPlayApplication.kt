7629453bf354f2935fa2db080f3af2f64feb01a5
Gleb Lazarev <g.lazarev@mcplat.ru>
2022-12-12 23:52:58 +0300

    Initial commit

---
+++ src/main/kotlin/com/dag/lets_play/LetsPlayApplication.kt
@@ -0,0 +1,11 @@
+package com.dag.lets_play
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LetsPlayApplication
+
+fun main(args: Array<String>) {
+    runApplication<LetsPlayApplication>(*args)
+}

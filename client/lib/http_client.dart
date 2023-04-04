a02861e77f406cc4e358cf2aa06b31f5555cf4a8
antongongelev <antongongelev@gmail.com>
2023-04-04 11:56:40 +0300

    add markers

---
+++ client/lib/http_client.dart
@@ -0,0 +1,12 @@
+import 'package:latlong2/latlong.dart';
+
+class LetsPlayHttpClient {
+  List<LatLng> stadiums = [
+    LatLng(59.990628, 30.309782),
+    LatLng(59.98903, 30.30577),
+  ];
+
+  List<LatLng> getStadiums() {
+    return stadiums;
+  }
+}
--- client/lib/map_page.dart
+++ client/lib/map_page.dart
@@ -1,7 +1,11 @@
 import 'package:client/navigation.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+
+import 'marker_with_tooltip.dart';
+import 'http_client.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({Key? key}) : super(key: key);
@@ -11,6 +15,30 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  LetsPlayHttpClient client = LetsPlayHttpClient();
+  List<Marker> markers = [];
+
+  void loadMarkers() {
+    List<Marker> result = [];
+
+    client.getStadiums().forEach((point) {
+      result.add(Marker(
+          point: point,
+          anchorPos: AnchorPos.align(AnchorAlign.center),
+          builder: (ctx) => MarkerWithTooltip(
+              tooltip: "the tooltip text", onTap: () => print("sfdasdfasdf"),
+              child: const Icon(CupertinoIcons.location_solid),
+          )));
+    });
+    markers = result;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadMarkers();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,11 +49,12 @@ class _MapPageState extends State<MapPage> {
             Flexible(
               child: FlutterMap(
                 options: MapOptions(
-                  center: LatLng(59.99137233759155, 30.309658542579047),
+                  center: LatLng(59.990628, 30.309782),
                   zoom: 14,
-                    minZoom: 10,
-                    maxZoom: 18,
-                  interactiveFlags: InteractiveFlag.all & ~InteractiveFlag.rotate
+                  minZoom: 10,
+                  maxZoom: 18,
+                  interactiveFlags:
+                      InteractiveFlag.all & ~InteractiveFlag.rotate,
                 ),
                 children: [
                   TileLayer(
@@ -33,6 +62,9 @@ class _MapPageState extends State<MapPage> {
                         'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
                     userAgentPackageName: 'com.dag.lets_play',
                   ),
+                  MarkerLayer(
+                    markers: markers,
+                  )
                 ],
               ),
             )
